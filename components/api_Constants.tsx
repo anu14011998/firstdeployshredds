7cfdc81a51ebef32848b322b5118f6fd43a4033c
InfodeltasysCompany <rahul.infodeltasys@gmail.com>
2023-12-11 12:13:47 +0530

    change in api_onstants comp

--- components/api_Constants.tsx
+++ components/api_Constants.tsx
@@ -1,1 +1,14 @@
-export const newurl = "newUrl";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const api_Constants = () => {
+  return (
+    <View>
+      <Text>new component created...</Text>
+    </View>
+  )
+}
+
+export default api_Constants
+
+const styles = StyleSheet.create({})
